71622df4d971d715d2555b069996028e38b0d669
khushihala19 <148927507+khushihala19@users.noreply.github.com>
2024-05-03 08:33:01 +0530

    Add files via upload

---
+++ java task/Task javas/3.2.js
@@ -0,0 +1,4 @@
+let a=" Name : khushi hala";
+let b="Age : 19";
+console.log(a,"\n",b);
+
---
+++ java task/Task javas/3.4.js
@@ -0,0 +1,11 @@
+let a=10,b=20,c;
+
+console.log("swap after..");
+console.log(a,b);
+
+c=a;
+a=b;
+b=c;
+
+console.log("swap before..");
+console.log(a,b);
